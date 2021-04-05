590ce88c33b11941e61cb4d07587680c45edfcec
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-05 00:47:11 +0000

    Add commas and include stdlib

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,15 @@
 # ...to a server without typing a password.
 # SSH client configuration configured to use the private key ~/.ssh/holberton
 # SSH client configuration configured to refuse to authenticate using a passwd
-
+include stdlib
 file_line { 'holbie private key':
-    ensure => present
+    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'remove pwd authentication':
-    ensure => present
+    ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
 }
