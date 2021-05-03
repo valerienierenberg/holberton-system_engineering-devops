18789055085ff40ef377bc98170c3e6abe182f6f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-03 20:43:01 +0000

    Move format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,18 @@ todo list progress """
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    userid = argv[1]
+if __name__ == '__main__':
+    userId = argv[1]
     name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(userid), verify=False).json()
+                        .format(userId), verify=False).json()
+    # todo variable = grabs all todos (completed or not) for the user passed in
     todo = requests.get('https://jsonplaceholder.typicode.com/todos?userid={}'
-                        .format(userid), verify=False).json()
+                        .format(userId), verify=False).json()
     completed = []
     for task in todo:
         if task.get('completed') is True:
             completed.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name.get('name'), len(completed), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed))
+    print("Employee {} is done with tasks({}/{}):".
+          format(name.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print("\t {}".format(task))
