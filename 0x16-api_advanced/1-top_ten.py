bc269ef609780aedd34599dc297c72da628784e1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-18 20:46:36 +0000

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit."""
+import requests
+from sys import argv
+import json
+
+
+def top_ten(subreddit):
+    subreddit = argv[1]
+    url = "http://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    u_agent = "Holberton-Reddit-API-project"
+
+    hot_response = requests.get(url, headers={'User-Agent': u_agent})
+
+    if hot_response.status_code != 200:
+        print(None)
+    else:
+        jsondata = hot_response.json()
+        hot = jsondata.get('data').get('children')
+        for new in hot:
+            title = new.get('data').get('title')
+            print(title)
